68e559370bcb565a8887fcab4fb97f4461063a5e
Seanan Chabra <Seanan Chabra>
2016-04-02 16:34:51 -0400

    fixed database calls

--- index.php
+++ index.php
@@ -26,11 +26,6 @@
     <!-- Custom styles for this template -->
     <link href="main.css" rel="stylesheet">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
@@ -74,15 +69,15 @@
               </thead>
               <tbody>
 
-                  <tr>
-                  <td><?php echo $first; ?></td>
-                  <td><?php echo $last; ?></td>
-                  <td><?php echo $phone; ?></td>
-                  <td><?php echo $acc; ?></td>
-                  <td><?php echo $twit; ?></td>
-                  <td><?php echo $status; ?></td>
-                  <td><button class="btn btn-xs btn-warning" type="submit">Create Transaction</button></td>
-                  </tr>
+                 <!--<tr>
+                  <td><?php //echo $first; ?></td>
+                  <td><?php //echo $last; ?></td>
+                  <td><?php //echo $phone; ?></td>
+                  <td><?php //echo $acc; ?></td>
+                  <td><?php //echo $twit; ?></td>
+                  <td><?php //echo $status; ?></td>
+                 <td><button class="btn btn-xs btn-warning" type="submit">Create Transaction</button></td>
+               </tr> -->
 
                   <tr>
                   <td>Test</td>
