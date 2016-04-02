fad4c80226cdd3333696fbac9828e1a6103b70db
Seanan Chabra <Seanan Chabra>
2016-04-02 19:01:42 -0400

    a

--- index.php
+++ index.php
@@ -58,10 +58,8 @@
                 $test = mysqli_query ($conn, "SELECT * FROM account_information");
                 var_dump($test);
                 echo "HELLO FAMILY!";
-                $test2 = $test->fetch_array(MYSQLI_ASSOC);
-                var_dump($test2);
-                foreach($test2 as $row){
-                  echo "S";
+                foreach($test as $row2){
+                  $row = $row2->fetch_array(MYSQLI_ASSOC);
                   var_dump($row);
                   $first = $row['first_name'];
                   $last = $row['last_name'];
