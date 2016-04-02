ce5cc9dd248938bc5a9f6f52dfffb8caa3beaadb
Seanan Chabra <Seanan Chabra>
2016-04-02 17:36:46 -0400

    b

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
                 define ('DB_NAME', 'Lockation');
                 define ('DB_USER', 'root');
                 define ('DB_PASSWORD', '.....1');
-                define ('DB_HOST', 'localhost');
+                define ('DB_HOST', '45.79.168.186');
 
                 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
                 if ($conn -> connect_error)
