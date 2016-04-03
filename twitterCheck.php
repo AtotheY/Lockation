5ea529aeff7a181d2e902095373f4ad834a701f8
AtotheY <sistillianthony@gmail.com>
2016-04-03 03:31:11 -0400

    fixed small problem with auth

--- twitterCheck.php
+++ twitterCheck.php
@@ -45,8 +45,8 @@ if ($conn -> connect_error)
       //$coord = $row['coordinates'];
       $geo = $row['geo'];
       $date = $row['created_at'];
-      $cords = $row['users']
-      var_dump($cords);
+      //$cords = $row['users']
+      //var_dump($cords);
       //echo "coord: " . $coord;
       echo "geo: " . $geo;
       var_dump($geo);
