9f0ba2ae6af44aad77c5081c72f83606ee6f8986
AtotheY <sistillianthony@gmail.com>
2016-04-02 21:44:02 -0400

    f

--- index.php
+++ index.php
@@ -77,12 +77,12 @@
                     $status = "Account Locked";
 
                   echo"<tr>";
-                  echo"<td allign = 'center'> ".$first." </td>";
-                  echo"<td> ".$last." </td>";
-                  echo"<td> ".$phone." </td>";
-                  echo"<td> ".$twit." </td>";
-                  echo"<td> ".$acc." </td>";
-                  echo"<td> ".$status." </td>";
+                  echo"<td align = 'center'".$first." </td>";
+                  echo"<td> align = 'center'".$last." </td>";
+                  echo"<td> align = 'center'".$phone." </td>";
+                  echo"<td> align = 'center'".$twit." </td>";
+                  echo"<td> align = 'center'".$acc." </td>";
+                  echo"<td> align = 'center'".$status." </td>";
                   if ($msg == 0)
                   {
                     echo"<td><form action ='transaction.php' method='post'/><button type = 'submit' value = '".$acc."class='btn btn-xs btn-warning' name ='acc'>Create Transaction</button>
