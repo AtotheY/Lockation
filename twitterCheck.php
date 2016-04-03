637bbe27b975c37301b9588340e9f4040a2690d5
AtotheY <sistillianthony@gmail.com>
2016-04-03 01:06:17 -0400

    added twitter code for basic pulling

--- twitterCheck.php
+++ twitterCheck.php
@@ -1,6 +1,14 @@
 <?php
-require_once('TwitterAPIExchange.php');
 
+function getTweet($handle)
+{
+
+
+require_once('TwitterAPIExchange.php');
+ini_set('display_errors', 1);
+ini_set('display_start;
+up_errors', 1);
+error_reporting(E_ALL);
 $settings = array(
     'oauth_access_token' => "	962554652-W3uFUXOMI6UYffcRTConjYBDY3QkcbqH6QBjqE0u",
     'oauth_access_token_secret' => "DaurNka3j8r9Yc6ALwIxMJdyJZLFGdVhweIRHnosL31Jw",
@@ -14,42 +22,15 @@ $settings = array(
 		// getting user Id's from previous page
 		$userOne = '&screen_name=' . $_POST['user1'];
 		$userTwo = '&screen_name=' . $_POST['user2'];
-		$url1 = 'https://api.twitter.com/1.1/friends/ids.json';
-		$url2 = 'https://api.twitter.com/1.1/friends/ids.json';
-		$getfield1 = "?cursor=-1";
-		$getfield1 .=  $userOne . "&count=5000";
-		$getfield2 = "?cursor=-1";
-		$getfield2 .= $userTwo . "&count=5000";
+		$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+		$getfield = '?screen_name='.$handle;
 		$requestMethod = 'GET';
-		ini_set('display_errors', 1);
-		ini_set('display_start;
-		up_errors', 1);
-		error_reporting(E_ALL);
-		$twitter = new TwitterAPIExchange($settings);
-		$response1 = $twitter->setGetfield($getfield1)->buildOauth($url1, $requestMethod)->performRequest();
-       		$response2 = $twitter->setGetfield($getfield2)->buildOauth($url2, $requestMethod)->performRequest();
-		$store1 = json_decode($response1, true);
-		$store2 = json_decode($response2, true);
-		//var_dump($store1);
-		//var_dump($response1);
-		$count = 0;
-		$ids1 = $store1['ids'];
-		$ids2 = $store2['ids'];
-		$url3 = 'https://api.twitter.com/1.1/users/show.json';
-		foreach ($ids1 as $data1) {
-			foreach ($ids2 as $data2)
-			{
-				if ($data1 == $data2)
-				{
-					$count++;
-					$getfield3 = "?user_id=".$data1;
-					$response3 = $twitter->setGetfield($getfield3)->buildOauth($url3, $requestMethod)->performRequest();
-					$store3 = json_decode($response3,true);
-					echo "Both users follow " . $store3['screen_id']."!";
-					//echo $data1. "is equal to" . $data2;
-					//echo "**";
-				}
-			}
-		}
-		echo "You have ".$count." friends in common!";
+
+    $twitter = new TwitterAPIExchange($settings);
+    $result =  $twitter->setGetfield($getfield)
+      ->buildOauth($url, $requestMethod)
+      ->performRequest();
+
+      var_dump($result);
+}
 	?>
