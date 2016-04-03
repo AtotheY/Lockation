ec0d220ecfdf5a8beec67161dddd58b0b83fda95
AtotheY <sistillianthony@gmail.com>
2016-04-03 08:31:21 -0400

    merged

--- index.php
+++ index.php
@@ -91,10 +91,10 @@ session_start();
 
                   }
                   else if ($msg == 1)
-                    echo"<td><form action = 'unwait.php'><button class='btn btn-xs btn-warning' value = '".$acc."' type='submit'>Clear Waiting</button></form></td></tr>";
+                    echo"<td><form action = 'unwait.php'><button class='btn btn-xs btn-warning' value = '".$acc."' name = 'acc' type='submit'>Clear Waiting</button></form></td></tr>";
                   else
                   {
-                        echo"<td><form action ='unlocked.php' method='post'/><button class='btn btn-xs btn-warning' value = '".$acc."'type='submit'>Unlock Account</button></form></td></tr>";
+                        echo"<td><form action ='unlocked.php' method='post'/><button class='btn btn-xs btn-warning' name = 'acc' value = '".$acc."'type='submit'>Unlock Account</button></form></td></tr>";
                   }
                 }
                 mysqli_close($conn);
