292927728b8db546877718aa0c3bafac999101c6
AtotheY <sistillianthony@gmail.com>
2016-04-03 05:01:35 -0400

    fixed small problem with auth

--- twilio.php
+++ twilio.php
@@ -9,8 +9,8 @@ $AuthToken = "113c57402a318e6335b997b2614a6217";
 $client = new Services_Twilio($AccountSid, $AuthToken);
 
 $message = $client->account->sendMessage->create(array(
-    "From" => "226-774-1587",
-    "To" => "416-319-9283",
+    "From" => "2267741587",
+    "To" => "4163199283",
     "Body" => "Test message!",
 ));
 
