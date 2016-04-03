d1466365c35b5d079fd4c0c6ae9290dc8019d7fd
AtotheY <sistillianthony@gmail.com>
2016-04-02 20:37:45 -0400

    f

--- index.php
+++ index.php
@@ -90,9 +90,9 @@
                   echo"<td> ".$twit." </td>";
                   echo"<td> ".$acc." </td>";
                   echo"<td> ".$status." </td>";
-                  if ($msg = 0)
+                  if ($msg == 0)
                     echo"<td><form action ='transaction.php' method='post'/><button class='btn btn-xs btn-warning' type='submit'>Create Transaction</button></form></td></tr>";
-                  else if ($msg = 1)
+                  else if ($msg == 1)
                     echo"<td><button class='btn btn-xs btn-warning' type='submit'>Clear Waiting</button></td></tr>";
                   else
                   {
