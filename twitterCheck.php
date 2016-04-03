1ac0f4071f404d063aee0712507576fe0c100809
AtotheY <sistillianthony@gmail.com>
2016-04-03 02:04:21 -0400

    fixed small problem with auth

--- twitterCheck.php
+++ twitterCheck.php
@@ -5,7 +5,7 @@ ini_set('display_start;
 up_errors', 1);
 error_reporting(E_ALL);
 $settings = array(
-    'oauth_access_token' => "	962554652-W3uFUXOMI6UYffcRTConjYBDY3QkcbqH6QBjqE0u",
+    'oauth_access_token' => "962554652-W3uFUXOMI6UYffcRTConjYBDY3QkcbqH6QBjqE0u",
     'oauth_access_token_secret' => "DaurNka3j8r9Yc6ALwIxMJdyJZLFGdVhweIRHnosL31Jw",
     'consumer_key' => "KdRTI0oBAIk94w7PiofD0cmo7",
     'consumer_secret' => "xIN5Musw80Rlip31To4Gnfx4cZUm2KEoH500hkoLoUL7gmil1y"
@@ -29,7 +29,7 @@ if ($conn -> connect_error)
     $row = $test->fetch_assoc();
 		// getting user Id's from previous page
 		$handle =  $row['twitter_account'];
-    echo "TEST2: handle : ". $handle;
+
   	$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 		$getfield = '?screen_name='.$handle;
 		$requestMethod = 'GET';
@@ -38,5 +38,7 @@ if ($conn -> connect_error)
     $result =  $twitter->setGetfield($getfield)
       ->buildOauth($url, $requestMethod)
       ->performRequest();
+
+
       var_dump($result);
 	?>
